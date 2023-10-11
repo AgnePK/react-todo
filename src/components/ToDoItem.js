9019fffc04fa1cd317672895c20600e99c23979a
Agne Postnikova <N00211108@iadt.ie>
2023-10-11 22:21:54 +0100

    ClassWork

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,5 +1,37 @@
 import ListGroup from "react-bootstrap/ListGroup";
+import Badge from "react-bootstrap/Badge";
 const ToDoItem = (props) => {
-	return <ListGroup.Item>{props.ToDo.text}</ListGroup.Item>;
+	let itemHTML = (
+		<>
+			{props.todo.text}
+			<Badge
+				onClick={() => props.markAsDone(props.todo.id)}
+				pill
+				bg="success"
+				className="float-end"
+			>
+				&#10003;
+			</Badge>
+			
+			<Badge
+				onClick={() => props.toDelete(props.todo.id)}
+				pill
+				bg="danger"
+				className="float-end"
+			>
+				&#10003;
+			</Badge>
+		</>
+	);
+	if (props.todo.done) {
+		itemHTML = (
+			<>
+				<span className="done">{props.todo.text}</span>
+				<span className="float-end">&#128077;</span>
+			</>
+		);
+	}
+
+	return <ListGroup.Item>{itemHTML}</ListGroup.Item>;
 };
 export default ToDoItem;
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,8 +1,9 @@
 import Card from "react-bootstrap/Card";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import ListGroup from "react-bootstrap/ListGroup";
 import ToDoItem from "./ToDoItem";
+
 const ToDoList = () => {
 	let initialList = [
 		{ id: 1, text: "Do laundry", done: true },
@@ -14,14 +15,60 @@ const ToDoList = () => {
 
 	const [list, setList] = useState(initialList);
 
-	const [textInput, setTextInput]= useState("")
+	const [textInput, setTextInput] = useState("");
 
-	const handleTextInput = (e) =>{
+	const handleTextInput = (e) => {
 		setTextInput(e.target.value);
-	}
+	};
+
+	const addToDoItem = () => {
+		let newTodo = {
+			id: list[list.length - 1].id + 1,
+			text: textInput,
+			done: false,
+		};
+
+		setList((prevList) => [...prevList, newTodo]);
+
+		setTextInput("");
+	};
+
+	const markAsDone = (id) => {
+		// console.log(id)
+		const newList = list.map((item) => {
+			if (item.id === id) {
+				item.done = true;
+			}
+			return item;
+		});
+
+		setList(newList);
+	};
+
+	const toDelete = (id) => {
+		const newList = list.filter((item) => item.id !== id);
+		setList(newList);
+	};
+
+	useEffect(() => {
+		localStorage.setList("list", JSON.stringify(list));
+	}, [list]);
+	useEffect(() => {
+		const list = JSON.parse(localStorage.getItem('list'));
+		if (list) {
+		 setList(list);
+		}
+	  }, []);
 
 	let ToDoItems = list.map((item) => {
-		return <ToDoItem key ={item.id} ToDo={item} />;
+		return (
+			<ToDoItem
+				key={item.id}
+				todo={item}
+				markAsDone={markAsDone}
+				toDelete={toDelete}
+			/>
+		);
 	});
 	return (
 		<Card>
@@ -31,7 +78,7 @@ const ToDoList = () => {
 			</Card.Body>
 			<Card.Footer>
 				<input type="text" onChange={handleTextInput} value={textInput} />
-				<Button variant="primary" className="float-end">
+				<Button variant="primary" className="float-end" onClick={addToDoItem}>
 					Add
 				</Button>
 			</Card.Footer>
